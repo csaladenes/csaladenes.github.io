dfd26992a3ad8a7e931560cb139da99c0312c925
Dénes Csala <mail@csaladen.es>
2019-01-28 10:35:28 +0000

    updates bi course links

---
+++ present/bi/projects/csoport/E/script.js
@@ -0,0 +1,71 @@
+function changeSize1(){
+    var d = document.getElementsByClassName('triangle1');
+    d[0].style.width = '700px';
+    d[0].style.height = '100px';
+
+}
+
+function reset1(){
+    document.getElementsByClassName('triangle1')[0].style.width = '600px';
+    document.getElementsByClassName('triangle1')[0].style.height = '100px';
+}
+
+function changeSize2(){
+    var d = document.getElementsByClassName('triangle2');
+    d[0].style.width = '600px';
+    d[0].style.height = '100px';
+
+}
+
+function reset2(){
+    document.getElementsByClassName('triangle2')[0].style.width = '500px';
+    document.getElementsByClassName('triangle2')[0].style.height = '100px';
+}
+
+function changeSize3(){
+    var d = document.getElementsByClassName('triangle3');
+    d[0].style.width = '500px';
+    d[0].style.height = '100px';
+
+}
+
+function reset3(){
+    document.getElementsByClassName('triangle3')[0].style.width = '400px';
+    document.getElementsByClassName('triangle3')[0].style.height = '100px';
+}
+
+function changeSize4(){
+    var d = document.getElementsByClassName('triangle4');
+    d[0].style.width = '400px';
+    d[0].style.height = '100px';
+
+}
+
+function reset4(){
+    document.getElementsByClassName('triangle4')[0].style.width = '300px';
+    document.getElementsByClassName('triangle4')[0].style.height = '100px';
+}
+
+function changeSize5(){
+    var d = document.getElementsByClassName('triangle5');
+    d[0].style.width = '300px';
+    d[0].style.height = '100px';
+
+}
+
+function reset5(){
+    document.getElementsByClassName('triangle5')[0].style.width = '200px';
+    document.getElementsByClassName('triangle5')[0].style.bottom = '60px';
+}
+
+function changeSize6(){
+    var d = document.getElementsByClassName('triangle6');
+    d[0].style.width = '200px';
+    d[0].style.height = '100px';
+
+}
+
+function reset6(){
+    document.getElementsByClassName('triangle6')[0].style.width = '100px';
+    document.getElementsByClassName('triangle6')[0].style.bottom = '-44px';
+}
